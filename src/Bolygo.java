9249a5562ca3d2235406f98a20c169da2a5a72e3
sanyi <sandor306@gmail.com>
2020-11-11 12:11:52 +0100

    Initial commit

---
+++ src/Bolygo.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public enum Bolygo {
+
+    MERKÚR,
+    VÉNUSZ,
+    FÖLD,
+    MARS,
+    JUPITER,
+    SZATURNUSZ,
+    URÁNUSZ,
+    NEPTUNUSZ,
+    PLUTÓ;
+    final ArrayList<Double> tomeg = new ArrayList<Double>() {
+    tomeg.add(3.30e+23);
+    tomeg.add(4.87e+24);
+    tomeg.add(4.87e+24);
+    tomeg.add(5.97e+24);
+    tomeg.add(6.42e+23);
+    tomeg.add(1.90e+27);
+    tomeg.add(5.68e+26);
+    tomeg.add(8.68e+25);
+    tomeg.add(5.98e+24);
+
+
+    };
+    ArrayList<Double> sugar= new ArrayList<Double>() {
+
+    sugar.add(2.44 0e6);
+    sugar.add(6.052e6);
+    sugar.add(6.378e6);
+    sugar.add(3.397e6);
+    sugar.add(7.1492e7);
+    sugar.add(2.5559e7);
+    sugar.add(6.0268e7);
+    sugar.add(2.4766e7);
+    sugar.add(1.37e6);
+
+
+    };
+}
